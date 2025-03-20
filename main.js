411dec77b8b15a4214704c63b817587cf71a3bf0
Charli <charli.vannood@gmail.com>
2025-03-20 14:19:33 +0100

    start quadtree

--- main.js
+++ main.js
@@ -4,11 +4,18 @@ const ctx = canvas.getContext("2d")
 canvas.width = window.innerWidth
 canvas.height = window.innerHeight
 
-var world = []
+var world = new World()
+console.log(world.getParticle(40, 40, world.world))
+console.log(world.setParticle(40, 70, world.world, 1))
+console.log(world.setParticle(41, 70, world.world, 1))
+console.log(world.setParticle(60, 30, world.world, 1))
 
 ctx.fillStyle = "#000000"
-for (x = 0; x < 16; x++) {
-    for (y = 0; y < 16; y++) {
-        ctx.fillRect(x * 10, y * 10, 10, 10)
+for (x = 0; x < 100; x++) {
+    for (y = 0; y < 100; y++) {
+        let type = world.getParticle(x, y, world.world)
+        if (type == 1) {
+            ctx.fillRect(x, y, 1, 1)
+        }
     }
 }
--- world.js
+++ world.js
@@ -1,5 +1,48 @@
 class World {
     constructor() {
-        
+        this.world = [{"size": 100, "x": 0, "y": 0, "value": 0, "children": []}]
+    }
+
+    getParticle(x, y, layer) {
+        for (let quad_ = 0; quad_ < layer.length; quad_++) {
+            let quad = layer[quad_]
+            if (quad["x"] <= x && quad["y"] <= y && quad["x"] + quad["size"] >= x && quad["y"] + quad["size"] >= y) {
+                if (quad["children"].length == 0) {
+                    return quad["value"]
+                } else {
+                    return this.getParticle(x, y, quad["children"])
+                }
+            }
+        }
+    }
+
+    setParticle(x, y, layer, value) {
+        if (Math.floor(layer[0]["size"] / 2) == 1) {
+            layer[0]["value"] = value
+            console.log("REACHED END")
+            return true
+        }
+
+        for (let quad_ = 0; quad_ < layer.length; quad_++) {
+            let quad = layer[quad_]
+            if (quad["x"] <= x && quad["y"] <= y && quad["x"] + quad["size"] >= x && quad["y"] + quad["size"] >= y) {
+                if (quad["children"].length == 0) {
+                    let chunk_x = Math.floor(x / Math.floor(quad["size"] / 2)) * Math.floor(quad["size"] / 2)
+                    let chunk_y = Math.floor(y / Math.floor(quad["size"] / 2)) * Math.floor(quad["size"] / 2)
+
+                    console.log(chunk_x, x)
+                    console.log(chunk_y, y)
+
+                    quad["children"].push({
+                        "size": Math.floor(quad["size"] / 2), 
+                        "x": chunk_x, "y": chunk_y, 
+                        "value": 0, "children": []
+                    })
+                }
+                let found = this.setParticle(x, y, quad["children"], value)
+                return found
+            }
+        }
+        console.log(this.world)
     }
 }
